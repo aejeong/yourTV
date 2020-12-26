44eb5cce69f0d72c747f0f87e82d773c5ce76d6f
aejeong <ehejrhc@gmail.com>
2020-12-26 23:53:30 +0900

    server listen

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('HI');
+const express = require('express');
+
+const port = 3000;
+const server = express()
+const serverListner = ()=>{
+    console.log('listeing on : http://localhost:'+port);
+}
+server.listen(port,'localhost',serverListner)
